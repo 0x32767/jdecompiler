15703d78500011a59dd2fe1a363efc49a2af2749
reirei <noreply+0x32767@github.com>
2026-06-13 17:01:39 +0000

    Added ConstantType

--- src/jdecompiler/class_file.py
+++ src/jdecompiler/class_file.py
@@ -1,11 +1,31 @@
-from pprint import pformat
 from dataclasses import dataclass
+from enum import IntEnum
 from io import BytesIO
-from typing import Any, Self, Optional
+from pprint import pformat
+from typing import Any
+from typing import Optional
+from typing import Self
+
+
+class ConstantType(IntEnum):
+    UTF_8 = 1
+    INTEGER = 3
+    FLOAT = 4
+    LONG = 5
+    DOUBLE = 6
+    CLASS = 7
+    STRING = 8
+    FIELD_REF = 9
+    METHOD_REF = 10
+    INTERFACE_METHOD_REF = 11
+    NAME_AND_TYPE = 12
+    METHOD_HANDLE = 15
+    METHOD_TYPE = 16
+    INVOKE_DYNAMIC = 18
 
 
 class ConstantPool:
-    def __init__(self, enteries: dict[int, tuple[str, Any, Any]]):
+    def __init__(self, enteries: dict[int, tuple[ConstantType, Any, Any]]):
         self._enteries = enteries
 
     def __str__(self):
@@ -17,33 +37,28 @@ class ConstantPool:
     def get(self, index):
         return self._enteries[index]
 
-    def resolve_class_info(self, index):
-        t, junk, idx = self.get(index)
-        assert t == "CLASS_INFO", str(self.get(index))
-        return self.get(idx)
-
     @property
     def enteries(self):
         return self._enteries.copy()
 
     @classmethod
     def from_buffer(cls, buffer: BytesIO, count: int):
-        enteries: dict[int, tuple[str, Any, Any]] = {}
+        enteries: dict[int, tuple[ConstantType, Any, Any]] = {}
 
         key = 0
         while key < count:
             key += 1
             tag = JavaClassFile.read_u1(buffer)
 
-            if tag == 1:
+            if tag == ConstantType.UTF_8:
                 # Reads a UTF-8 String
                 length = JavaClassFile.read_u2(buffer)
-                enteries[key] = ("UTF-8", 0, buffer.read(length))
+                enteries[key] = (ConstantType.UTF_8, 0, buffer.read(length))
 
-            elif tag == 6:
+            elif tag == ConstantType.DOUBLE:
                 # Reads a number of type double
                 enteries[key] = (
-                    "DOUBLE",
+                    ConstantType.DOUBLE,
                     JavaClassFile.read_u4(buffer),
                     JavaClassFile.read_u4(buffer),
                 )
@@ -51,56 +66,64 @@ class ConstantPool:
                 # so the key is incremented here
                 key += 1
 
-            elif tag == 7:
+            elif tag == ConstantType.CLASS:
                 # Class info contains a string with the name of the class
-                enteries[key] = ("CLASS_INFO", None, JavaClassFile.read_u2(buffer))
+                enteries[key] = (
+                    ConstantType.CLASS,
+                    None,
+                    JavaClassFile.read_u2(buffer),
+                )
 
-            elif tag == 8:
+            elif tag == ConstantType.STRING:
                 # String points to a UTF-8 string
-                enteries[key] = ("STRING", None, JavaClassFile.read_u2(buffer))
+                enteries[key] = (
+                    ConstantType.STRING,
+                    None,
+                    JavaClassFile.read_u2(buffer),
+                )
 
-            elif tag == 9:
+            elif tag == ConstantType.FIELD_REF:
                 # Points to a class and field belonging to that class
                 enteries[key] = (
-                    "FIELD_REF",
+                    ConstantType.FIELD_REF,
                     JavaClassFile.read_u2(buffer),
                     JavaClassFile.read_u2(buffer),
                 )
 
-            elif tag == 10:
+            elif tag == ConstantType.METHOD_REF:
                 # Points to a class and method belonging to that class
                 enteries[key] = (
-                    "METHOD_REF",
+                    ConstantType.METHOD_REF,
                     JavaClassFile.read_u2(buffer),
                     JavaClassFile.read_u2(buffer),
                 )
 
-            elif tag == 11:
+            elif tag == ConstantType.INTERFACE_METHOD_REF:
                 # Points to a class and interface used in that class
                 enteries[key] = (
-                    "INTERFACE_REF",
+                    ConstantType.INTERFACE_METHOD_REF,
                     JavaClassFile.read_u2(buffer),
                     JavaClassFile.read_u2(buffer),
                 )
 
-            elif tag == 12:
+            elif tag == ConstantType.NAME_AND_TYPE:
                 # Points to a string and type
                 enteries[key] = (
-                    "NAME_AND_TYPE_REF",
+                    ConstantType.NAME_AND_TYPE,
                     JavaClassFile.read_u2(buffer),
                     JavaClassFile.read_u2(buffer),
                 )
 
-            elif tag == 15:
+            elif tag == ConstantType.METHOD_HANDLE:
                 enteries[key] = (
-                    "METHOD_HANDLE",
+                    ConstantType.METHOD_HANDLE,
                     JavaClassFile.read_u1(buffer),
                     JavaClassFile.read_u2(buffer),
                 )
 
-            elif tag == 18:
+            elif tag == ConstantType.INVOKE_DYNAMIC:
                 enteries[key] = (
-                    "INVOKE_DYNAMIC",
+                    ConstantType.INVOKE_DYNAMIC,
                     JavaClassFile.read_u2(buffer),
                     JavaClassFile.read_u2(buffer),
                 )
@@ -120,7 +143,7 @@ class Attribute:
     def from_buffer_and_pool(cls, buffer: BytesIO, constant_pool: ConstantPool):
         name_index = JavaClassFile.read_u2(buffer)
         name_type, _, name = constant_pool.get(name_index)
-        assert name_type == "UTF-8"
+        assert name_type == ConstantType.UTF_8
 
         length = JavaClassFile.read_u4(buffer)
         data = buffer.read(length)
@@ -140,10 +163,10 @@ class Method:
         access_flags = JavaClassFile.read_u2(buffer)
 
         name_type, _, name = constant_pool.get(JavaClassFile.read_u2(buffer))
-        assert name_type == "UTF-8"
+        assert name_type == ConstantType.UTF_8
 
         name_type, _, descriptor = constant_pool.get(JavaClassFile.read_u2(buffer))
-        assert name_type == "UTF-8"
+        assert name_type == ConstantType.UTF_8
 
         attribute_count = JavaClassFile.read_u2(buffer)
 
@@ -201,8 +224,12 @@ class JavaClassFile:
         constant_pool = ConstantPool.from_buffer(buffer, count=constant_pool_count - 1)
 
         access_flags = cls.read_u2(buffer)
-        this_class = constant_pool.resolve_class_info(cls.read_u2(buffer))
-        super_class = constant_pool.resolve_class_info(cls.read_u2(buffer))
+
+        data_type, _, this_class = constant_pool.get(cls.read_u2(buffer))
+        assert data_type == ConstantType.CLASS
+
+        data_type, _, super_class = constant_pool.get(cls.read_u2(buffer))
+        assert data_type == ConstantType.CLASS
 
         interface_count = cls.read_u2(buffer)
         interfaces: list = []
