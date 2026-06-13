9f44c85256d4c3f2f8e9c190f02213faa557d879
reirei <noreply+0x32767@github.com>
2026-06-13 17:10:42 +0000

    Removed ConstantPool

--- src/jdecompiler/class_file.py
+++ src/jdecompiler/class_file.py
@@ -24,25 +24,87 @@ class ConstantType(IntEnum):
     INVOKE_DYNAMIC = 18
 
 
-class ConstantPool:
-    def __init__(self, enteries: dict[int, tuple[ConstantType, Any, Any]]):
-        self._enteries = enteries
-
-    def __str__(self):
-        return f"{type(self).__name__}({pformat(self.enteries)})"
-
-    def __repr__(self):
-        return str(self)
-
-    def get(self, index):
-        return self._enteries[index]
-
-    @property
-    def enteries(self):
-        return self._enteries.copy()
+@dataclass
+class Attribute:
+    name: str
+    info: bytes
 
     @classmethod
-    def from_buffer(cls, buffer: BytesIO, count: int):
+    def from_buffer_and_pool(cls, buffer: BytesIO, constant_pool: dict[int, tuple]):
+        name_index = JavaClassFile.read_u2(buffer)
+        name_type, _, name = constant_pool[name_index]
+        assert name_type == ConstantType.UTF_8
+
+        length = JavaClassFile.read_u4(buffer)
+        data = buffer.read(length)
+
+        return cls(name=name, info=data)
+
+
+@dataclass
+class Method:
+    access_flags: int
+    name: str
+    descriptor: str
+    attributes: list
+
+    @classmethod
+    def from_buffer_and_pool(cls, buffer: BytesIO, constant_pool: dict[int, tuple]):
+        access_flags = JavaClassFile.read_u2(buffer)
+
+        name_type, _, name = constant_pool[JavaClassFile.read_u2(buffer)]
+        assert name_type == ConstantType.UTF_8
+
+        name_type, _, descriptor = constant_pool[JavaClassFile.read_u2(buffer)]
+        assert name_type == ConstantType.UTF_8
+
+        attribute_count = JavaClassFile.read_u2(buffer)
+
+        attributes = []
+        for _ in range(attribute_count):
+            attributes.append(Attribute.from_buffer_and_pool(buffer, constant_pool))
+
+        return cls(
+            access_flags=access_flags,
+            name=name,
+            descriptor=descriptor,
+            attributes=attributes,
+        )
+
+
+@dataclass
+class JavaClassFile:
+    magic: bytes  # magic number at start of java class file must be 0xCAFEBABE
+    minor_version: int
+    major_version: int
+    constant_pool: list
+    access_flags: int
+    this_class: int
+    super_class: int
+    interfaces: list
+    field_info: list
+    methods: list
+    attributes: list
+
+    @staticmethod
+    def read_u4(buffer: BytesIO):
+        return int.from_bytes(buffer.read(4), byteorder="big", signed=False)
+
+    @staticmethod
+    def read_u2(buffer: BytesIO):
+        return int.from_bytes(buffer.read(2), byteorder="big", signed=False)
+
+    @staticmethod
+    def read_u1(buffer: BytesIO):
+        return int.from_bytes(buffer.read(1), byteorder="big", signed=False)
+
+    @classmethod
+    def from_file(cls, fp):
+        with open(fp, "rb") as f:
+            return cls.from_buffer(f)
+
+    @staticmethod
+    def read_constant_pool(buffer, count):
         enteries: dict[int, tuple[ConstantType, Any, Any]] = {}
 
         key = 0
@@ -131,87 +193,7 @@ class ConstantPool:
             else:
                 raise NotImplementedError(tag)
 
-        return cls(enteries)
-
-
-@dataclass
-class Attribute:
-    name: str
-    info: bytes
-
-    @classmethod
-    def from_buffer_and_pool(cls, buffer: BytesIO, constant_pool: ConstantPool):
-        name_index = JavaClassFile.read_u2(buffer)
-        name_type, _, name = constant_pool.get(name_index)
-        assert name_type == ConstantType.UTF_8
-
-        length = JavaClassFile.read_u4(buffer)
-        data = buffer.read(length)
-
-        return cls(name=name, info=data)
-
-
-@dataclass
-class Method:
-    access_flags: int
-    name: str
-    descriptor: str
-    attributes: list
-
-    @classmethod
-    def from_buffer_and_pool(cls, buffer: BytesIO, constant_pool: ConstantPool):
-        access_flags = JavaClassFile.read_u2(buffer)
-
-        name_type, _, name = constant_pool.get(JavaClassFile.read_u2(buffer))
-        assert name_type == ConstantType.UTF_8
-
-        name_type, _, descriptor = constant_pool.get(JavaClassFile.read_u2(buffer))
-        assert name_type == ConstantType.UTF_8
-
-        attribute_count = JavaClassFile.read_u2(buffer)
-
-        attributes = []
-        for _ in range(attribute_count):
-            attributes.append(Attribute.from_buffer_and_pool(buffer, constant_pool))
-
-        return cls(
-            access_flags=access_flags,
-            name=name,
-            descriptor=descriptor,
-            attributes=attributes,
-        )
-
-
-@dataclass
-class JavaClassFile:
-    magic: bytes  # magic number at start of java class file must be 0xCAFEBABE
-    minor_version: int
-    major_version: int
-    constant_pool: list
-    access_flags: int
-    this_class: int
-    super_class: int
-    interfaces: list
-    field_info: list
-    methods: list
-    attributes: list
-
-    @staticmethod
-    def read_u4(buffer: BytesIO):
-        return int.from_bytes(buffer.read(4), byteorder="big", signed=False)
-
-    @staticmethod
-    def read_u2(buffer: BytesIO):
-        return int.from_bytes(buffer.read(2), byteorder="big", signed=False)
-
-    @staticmethod
-    def read_u1(buffer: BytesIO):
-        return int.from_bytes(buffer.read(1), byteorder="big", signed=False)
-
-    @classmethod
-    def from_file(cls, fp):
-        with open(fp, "rb") as f:
-            return cls.from_buffer(f)
+        return enteries
 
     @classmethod
     def from_buffer(cls, buffer) -> Self:
@@ -221,7 +203,7 @@ class JavaClassFile:
         major_version = cls.read_u2(buffer)
 
         constant_pool_count = cls.read_u2(buffer)
-        constant_pool = ConstantPool.from_buffer(buffer, count=constant_pool_count - 1)
+        constant_pool = cls.read_constant_pool(buffer, constant_pool_count - 1)
 
         access_flags = cls.read_u2(buffer)
 
